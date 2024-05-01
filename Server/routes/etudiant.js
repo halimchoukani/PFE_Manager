10a1d5c38879c9054b5e2c79317fdd3a55df35e9
Halim Choukani <halim.choukani@gmail.com>
2024-05-01 22:50:56 +0100

    Add CIN model and secret key constant in etudiant.js

--- Server/routes/etudiant.js
+++ Server/routes/etudiant.js
@@ -4,7 +4,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const Etudiant = require("../models/etudiant");
-
+const CIN = require("../models/cin");
+const secretKey = "gozgjzgpojzrpojz";
+const Stage = require("../models/stage");
 // Multer configuration
 const storage = multer.memoryStorage();
 const upload = multer({
@@ -26,21 +28,28 @@ const upload = multer({
 router.post("/register", async (req, res) => {
   try {
     const { nom, prenom, cin, email, password, room } = req.body;
-    const existingEtudiant = await Etudiant.findOne({ email });
-    if (existingEtudiant) {
-      return res.status(400).send("email deja existe");
+    const exist = CIN.findOne({ cin: cin });
+    if (exist) {
+      const existingEtudiant = await Etudiant.findOne({ email });
+      if (existingEtudiant) {
+        return res.status(400).send("email deja existe");
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const etudiant = new Etudiant({
+        nom,
+        prenom,
+        cin,
+        email,
+        password: hashedPassword,
+        room,
+      });
+      await etudiant.save();
+      res.status(201).send(etudiant);
+    } else {
+      res
+        .status(404)
+        .send("Vous n'avez pas l'access a cette platform , contacter l'admin");
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const etudiant = new Etudiant({
-      nom,
-      prenom,
-      cin,
-      email,
-      password: hashedPassword,
-      room,
-    });
-    await etudiant.save();
-    res.status(201).send(etudiant);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -52,14 +61,14 @@ router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const etudiant = await Etudiant.findOne({ email });
     if (!etudiant) {
-      return res.status(404).send("Etudiant not found");
+      return res.status(404).send("Etudiant n'existe pas");
     }
     const isPasswordMatch = await bcrypt.compare(password, etudiant.password);
     if (!isPasswordMatch) {
-      return res.status(401).send("Invalid password");
+      return res.status(401).send("Email out mot de passe incorrecte");
     }
-    const token = jwt.sign({ id: etudiant._id }, "your_secret_key", {
-      expiresIn: "1h",
+    const token = jwt.sign({ id: etudiant._id }, secretKey, {
+      expiresIn: "24h",
     });
     res.send({ token });
   } catch (error) {
@@ -68,24 +77,29 @@ router.post("/login", async (req, res) => {
 });
 
 // Create Stage PFE
-router.post("/stage", upload.single("fichier"), async (req, res) => {
+router.post("/ajouterstage", upload.single("fichier"), async (req, res) => {
   try {
-    const { sujet, description, date_debut, date_fin } = req.body;
-    const etudiantId = req.user.id; // Extracted from JWT token
-    const etudiant = await Etudiant.findById(etudiantId);
-    if (!etudiant) {
-      return res.status(404).send("Etudiant not found");
+    const etudiant = req.body;
+    const et = await Etudiant.findOne({ cin: etudiant.cin });
+    const binome = await Etudiant.findOne({ cin: etudiant.binome });
+    if (!et) {
+      return res.status(404).send("Etudiant n'existe pas");
     }
-    const stage = {
-      sujet,
-      description,
-      date_debut,
-      date_fin,
-      fichier: req.file.buffer, // Store file buffer
-    };
-    etudiant.stage_pfe = stage;
-    await etudiant.save();
-    res.status(201).send(etudiant.stage_pfe);
+    if (!binome) {
+      return res.status(404).send("Binome n'existe pas");
+    }
+    const stage = Stage.findOne({ etudiant: et._id });
+    const s = new Stage();
+    if (stage) {
+      stage.findOneAndUpdate(
+        { etudiant: et._id },
+        { binome: binome._id },
+        etudiant
+      );
+      return res.status(200).send("Stage modifié avec succès");
+    }
+    stage.save();
+    res.status(201).send("Stage ajouté avec succès");
   } catch (error) {
     res.status(500).send(error.message);
   }
