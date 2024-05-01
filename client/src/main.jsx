118e9a3e3b0d08f76842a3c3f265806216956799
Halim Choukani <halim.choukani@gmail.com>
2024-05-01 01:45:20 +0100

    Update Tailwind CSS configuration and add Material Tailwind theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
+import { ThemeProvider } from "@material-tailwind/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindConfig from './tailwind.config'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindConfig],
-})
+  plugins: [react(), withMT],
+});
